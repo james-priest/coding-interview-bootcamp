53a28997c82216d806c06de355c593d9c2e463c2
James Priest <james-priest@outlook.com>
2019-08-03 15:16:05 -0700

    feat: Add Linked List from last algorithm

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,16 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+  let slow = list.getFirst();
+  let fast = list.getAt(n);
+
+  while (fast.next) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  return slow;
+}
 
 module.exports = fromLast;
